559b857b965e055b6bab248be4aec0a52d694187
Henrique <henriqueb.hahn2000@gmail.com>
2021-08-19 21:39:15 -0300

    Melhorando o código

--- C/1036/main.c
+++ C/1036/main.c
@@ -1,19 +1,20 @@
-//a, b, c, r1, r2, raizQ = número 1, 2 e 3, resultado 1 e 2, raiz quadrada
+//a, b, c, r1, r2, delta = número 1, 2 e 3, resultado 1 e 2, raiz quadrada
 #include <stdio.h>
-#include<math.h>
+#include <math.h>
 
 int main() {
-  double a, b, c, r1, r2, raizQ;
+  double a, b, c, r1, r2;
   scanf("%lf %lf %lf", &a, &b, &c);
-  raizQ = sqrt(b*b-4*a*c);
-  if(a != 0 && raizQ >= 0){
-    r1 = (-b+raizQ)/(2*a);
-    r2 = (-b-raizQ)/(2*a);
-    printf("R1 = %.5lf\n", r1);
-    printf("R2 = %.5lf\n", r2);
+  double delta = b*b-4*a*c;
+  if(a == 0 && delta < 0){
+    printf("Impossivel calcular\n");
   }
   else{
-    printf("Impossivel calcular\n");
+    delta = sqrt(delta);
+    r1 = (-b+delta)/(2*a);
+    r2 = (-b-delta)/(2*a);
+    printf("R1 = %.5lf\n", r1);
+    printf("R2 = %.5lf\n", r2);
   }
   return 0;
 }
