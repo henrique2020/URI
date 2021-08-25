d1d4d510176ebc00f4720fba31962049e4d46df5
Henrique <henriqueb.hahn2000@gmail.com>
2021-08-25 10:39:38 -0300

    Q 1013

---
+++ C/1013/main.c
@@ -0,0 +1,12 @@
+//a, b, c = números 1, 2 e 3
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b, c, maior;
+    scanf("%d %d %d", &a, &b, &c);
+    maior = (a+b+abs(a-b))/2;
+    maior = (maior+c+abs(maior-c))/2;
+    printf("%d eh o maior\n", maior);
+    return 0;
+}
