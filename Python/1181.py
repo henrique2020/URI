5ccd9cee606ddbdff5e5d497e2b7b66f0408f6a4
Henrique <48599271+henrique2020@users.noreply.github.com>
2021-12-28 10:25:28 -0300

    Linha na Matriz

---
+++ Python/1181.py
@@ -0,0 +1,15 @@
+pos = int(input())
+ope = input()
+
+soma = 0.0
+for linha in range(12):
+    for coluna in range(12):
+        valor = float(input())
+        if linha == pos:
+            soma += valor
+
+if ope == 'S':
+    print(round(soma, 1))
+
+else:
+    print(round((soma/12), 1))
