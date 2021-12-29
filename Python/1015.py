fc1b1486b66835926fc8ab824b75af3deda8312f
Henrique <48599271+henrique2020@users.noreply.github.com>
2021-12-29 09:57:35 -0300

    Distância Entre Dois Pontos

---
+++ Python/1015.py
@@ -0,0 +1,12 @@
+import math
+
+x1, y1= input().split()
+x1 = float(x1)
+y1 = float(y1)
+
+x2, y2= input().split()
+x2 = float(x2)
+y2 = float(y2)
+
+distancia = math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+print("%.4f" % distancia);
