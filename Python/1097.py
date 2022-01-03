a2c578892845711c409005b568d57449e7afd3f2
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-03 15:48:11 -0300

    Sequencia IJ 3

--- Python/1097.py
+++ Python/1097.py
@@ -1,3 +1,5 @@
+#Método zoado
+'''
 print('I=1 J=7')
 print('I=1 J=6')
 print('I=1 J=5')
@@ -13,3 +15,10 @@ print('I=7 J=11')
 print('I=9 J=15')
 print('I=9 J=14')
 print('I=9 J=13')
+'''
+
+
+#Método certo
+for i in range(1, 10, 2):
+    for s in range(6, 3, -1):
+        print('I=%d J=%d' % (i, i+s))
