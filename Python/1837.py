cc3dc62d4449b545d4f73ab7172045fe2772c475
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-05 10:48:40 -0300

    Prefácio

--- Python/1837.py
+++ Python/1837.py
@@ -1,3 +1,4 @@
+## Primeira versão - 0.316s
 linha = input().split()
 a, b = linha
 
@@ -7,3 +8,10 @@ resto = a%abs(b)
 quociente = (a-resto)/b
 
 print("{:d} {:d}".format(round(quociente), resto))
+
+##Segunda versão - 0.024s
+a, b = map(int, input().split())
+resto = a%abs(b)
+quociente = int((a-resto)/b)
+
+print(quociente, resto)
