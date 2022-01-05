4ed42013c131f17c94025e8c258c537efa9c9780
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-05 11:18:42 -0300

    Vende-se

--- Python/2433.py
+++ Python/2433.py
@@ -2,13 +2,13 @@ pred = list(map(int, input().split()))
 dis_pred = list(map(int, input().split()))
 dis_pred.sort()
 
-restam = pred[0]-pred[1]
+restam = pred[0]-pred[1]-1
 
-soma = dis_pred[(restam-1)] - dis_pred[0]
+soma = dis_pred[restam] - dis_pred[0]
 distancia = soma
 
-for x in range(1, pred[1]):
-    soma = dis_pred[x+(restam-1)] - dis_pred[x]
+for x in range(1, pred[1]+1):
+    soma = dis_pred[x+restam] - dis_pred[x]
     if(soma < distancia):
         distancia = soma
 
