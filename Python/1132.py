76aed708182cfc6008936def75f51c1c20e29f09
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-05 10:13:03 -0300

    Múltiplos de 13

--- Python/1132.py
+++ Python/1132.py
@@ -1,3 +1,4 @@
+## Primeira versão - 0.456s
 arr = []
 arr.append(int(input()))
 arr.append(int(input()))
@@ -8,4 +9,15 @@ while ini <= fim:
     if ini%13 != 0:
         soma+=ini
     ini+=1
+print(soma)
+
+##Segunda versão - 0.111s
+arr = []
+arr.append(int(input()))
+arr.append(int(input()))
+arr.sort()
+soma = 0
+for x in range(arr[0], arr[1]+1):
+    if x%13 != 0:
+        soma+=x
 print(soma)
