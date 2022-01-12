33ecb1bc208176a873e62c9795446564fb936b63
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-11 21:35:20 -0300

    Acima da Diagonal Secundária

---
+++ Python/1185.py
@@ -0,0 +1,14 @@
+ope = input()
+
+soma = 0.0
+for linha in range(12):
+    for coluna in range(11, -1, -1):
+        valor = float(input())
+        if coluna > linha:
+            soma += valor
+
+if ope == 'S':
+    print(round(soma, 1))
+
+else:
+    print(round((soma/66), 1)) #66 é o número de casas acima da diagonal
