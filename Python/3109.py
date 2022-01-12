cf52b05edab274ff1c786b5ce9947aa6d68452e9
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-01-12 14:01:36 -0300

    Trocando de Mesa

--- Python/3109.py
+++ Python/3109.py
@@ -1,3 +1,4 @@
+# 35% X
 func = int(input())
 mesa = []
 for x in range(func):
@@ -13,4 +14,26 @@ while rep:
         mesa[pos2] = event[1]
     else:
         print(mesa.index(event[1]))
+    rep-=1
+    
+
+## Um caminho pra solução
+func = int(input())
+mesa = {}
+for x in range(func):
+    mesa[x+1] = 0
+
+rep = int(input())
+while rep:
+    event = list(map(int, input().split()))
+    if event[0] == 1:
+        calc = len(mesa) - abs(event[1]-event[2]) + mesa[event[1]]
+        print(mesa)
+        print(mesa[event[1]])
+        print(calc)
+        mesa[event[1]] = calc
+        mesa[event[2]] = calc
+        print(mesa)
+    else:
+        print(mesa[event[1]])
     rep-=1
