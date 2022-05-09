8290492d045bb7d8dd50a1617175022da0dce8cf
henrique2020 <henriqueb.hahn2000@gmail.com>
2022-05-09 20:07:55 -0300

    Área Superior

---
+++ Python/1187.py
@@ -0,0 +1,14 @@
+ope = input()
+
+soma = 0.0
+for linha in range(12):
+    for coluna in range(12):
+        valor = float(input())
+        if (linha != coluna and coluna > linha) and linha+coluna < 11:
+            soma += valor
+            
+if ope == 'S':
+    print(round(soma, 1))
+
+else:
+    print(round((soma/30), 1)) #66 é o número de casas na área superior
