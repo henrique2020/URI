ae9952a113cefac0a0b41683cf92f22d9a43d56c
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-07-05 12:37:11 -0300

    Super Primos: Ativar!

--- Python/2674.py
+++ Python/2674.py
@@ -1,36 +1,24 @@
-def verificaPrimo(numero):
-    info = True
-    start = 2
-
-    if numero == 1 or numero == 0:
-        info = False
-
-    else:
-        while(start <= numero/2):
-            if numero % start == 0:
-                info = False
-                break
-            start += 1
-    return info
-
+def primo(n):
+    if (n%2 == 0 and n>2) or n<2:
+        return False
+    return all(n%i for i in range(3,int((n**0.5)+1), 2))
 while True:
-    try: n = abs(int(input()))
-    except: break
-    num = list(map(int, str(n)))
-
-    primo = False
-    if(verificaPrimo(n)):
-        primo = True
-        super = True
-        for x in num:
-            if(verificaPrimo(x) is False):
-                super = False
-                break
-
-    if(primo):
-        if(super):
-            print('Super')
-        else:
-            print('Primo')
-    else:
-        print('Nada')
+    try:
+        num = input()
+        if primo(int(num)):
+            superP = True
+            for e in num:
+                if primo(int(e)):
+                    pass
+                else:
+                    superP = False
+                    break
+                
+            if superP: 
+                print("Super")
+            else: 
+                print("Primo")
+        else: 
+            print("Nada")
+    except:
+        break
