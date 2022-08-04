2baeabe2c33ee75c8a4622f049527b4fa698b4f5
Henrique Hahn <henriqueb.hahn2000@gmail.com>
2022-08-03 23:17:05 -0300

    Esquerda, Volver!

--- Java/src/ex1437/Main.java
+++ Java/src/ex1437/Main.java
@@ -1,18 +1,13 @@
 package ex1437;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		File arquivo = new File("Entrada/1437");
-		//InputStreamReader ir = new InputStreamReader(System.in);
-		FileReader ir = new FileReader(arquivo);
+		InputStreamReader ir = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(ir);
 		String linha;
 		while(true){
