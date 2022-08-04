90d1cb6cef5d8e967ceccebc054ad3f8d887398e
Henrique Hahn <henriqueb.hahn2000@gmail.com>
2022-08-03 23:11:24 -0300

    Extremamente Básico

--- Java/src/ex1001/Main.java
+++ Java/src/ex1001/Main.java
@@ -1,18 +1,13 @@
 package ex1001;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-        
-		File arquivo = new File("Entrada/1001");
-		//InputStreamReader ir = new InputStreamReader(System.in);
-    	FileReader ir = new FileReader(arquivo);
+		InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ir);
     	        
         int A = 0, B = 0, X = 0;
