df13ddbe4808b1e8f1f7d5913ad2b7e4e93aa02a
Henrique Hahn <henriqueb.hahn2000@gmail.com>
2022-08-03 23:11:36 -0300

    Área do Círculo

--- Java/src/ex1002/Main.java
+++ Java/src/ex1002/Main.java
@@ -1,18 +1,13 @@
 package ex1002;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
      
     public static void main(String[] args) throws IOException {
-		
-    	File arquivo = new File("Entrada/1002");
-		//InputStreamReader ir = new InputStreamReader(System.in);
-    	FileReader ir = new FileReader(arquivo);
+		InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ir);
         
 		double r = Double.parseDouble(in.readLine());
