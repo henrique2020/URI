a08667098c74abb81293fb9c656eae821f5fba1a
Henrique Hahn <henriqueb.hahn2000@gmail.com>
2022-08-03 23:14:29 -0300

    Libertadores

--- Java/src/ex1536/Main.java
+++ Java/src/ex1536/Main.java
@@ -1,7 +1,6 @@
 package ex1536;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -9,8 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		//InputStreamReader ir = new InputStreamReader(System.in);
-		FileReader ir = new FileReader("Entrada/1536");
+		InputStreamReader ir = new InputStreamReader(System.in);
 		BufferedReader in = new BufferedReader(ir);
 		
 		String linha = in.readLine();
