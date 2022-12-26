6b9c627116891ecb55a2890258cbdbb9d110480e
Henrique <48599271+henrique2020@users.noreply.github.com>
2022-12-26 14:40:56 -0300

    Time limit exceeded

--- Python/1856.py
+++ Python/1856.py
@@ -1,57 +1,18 @@
-'''
-    { TIME LIMIT EXCEEDED }
-'''
-##Modelo 1
-def add(p, e):
-    for x in lista:
-        if(x == e):
-            lista.insert(lista.index(x)+1, p)
-            break
-
-def delete(e):
-    lista.remove(e)
-
-def quantidade(ini, fim):
-    posIni = lista.index(ini)
-    posFim = lista.index(fim)
-    if(posIni > posFim):
-        dif = posIni-1 - posFim
-    else:
-        dif = posFim-1 - posIni
-    results.append(dif)
-    print('{:d}'.format(dif))
-
-inimigos = int(input())
-lista = input().split()
-operacoes = int(input())
-results = []
-for rep in range(operacoes):
+input()
+inimigos = input().split()
+rep = int(input())
+for _ in range(rep):
     dados = input().split()
-
-    if(dados[0] == 'I'):
-        add(dados[1], dados[2])
-    elif(dados[0] == 'R'):
-        delete(dados[1])
+    op = dados[0]
+    
+    if(op == 'I'):
+        p = dados[1]
+        e = dados[2]
+        inimigos.insert(inimigos.index(e)+1, p)
+    elif(op == 'R'):
+        e = dados[1]
+        inimigos.remove(e)
     else:
-        quantidade(dados[1], dados[2])
-
-##Modelo de teste 2
-'''
-inimigos = int(input())
-lista = input().split()
-operacoes = int(input())
-for rep in range(operacoes):
-    dados = input().split()
-    if(dados[0] == 'I'):
-        lista.insert(lista.index(dados[2])+1, dados[1])
-    elif(dados[0] == 'R'):
-        lista.remove(dados[1])
-    else:
-        posIni = lista.index(dados[1])
-        posFim = lista.index(dados[2])
-        if(posIni > posFim):
-            dif = posIni-1 - posFim
-        else:
-            dif = posFim-1 - posIni
-        print('%d' % dif)
-'''
+        ini = dados[1]
+        fim = dados[2]
+        print('{:d}'.format(abs(inimigos.index(ini) - inimigos.index(fim))-1))
