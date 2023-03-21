f4c1cf60d68baeaf41d1ff6d7788b558d8baadf0
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-03-21 16:12:41 -0300

    Triângulo

--- Python/1929.py
+++ Python/1929.py
@@ -1,8 +1,4 @@
 varetas = list(map(int, input().split()))
 varetas.sort()
-
-if(abs(varetas[0] - varetas[1]) > abs(varetas[-2] - varetas[-1])): a, b, c = varetas[:3]
-else: a, b, c = varetas[1:]
-
-if(a+b>=c) and (b+c>=a) and (c+a>=b): print('S')
+if(varetas[0]+varetas[1] > varetas[2]) or (varetas[1]+varetas[2] > varetas[3]): print('S')
 else: print('N')
