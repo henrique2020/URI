13883c4d18dae39cf1efc432ff19022e0a81c084
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-04-05 15:18:11 -0300

    Matriz Quadrada I

--- Python/1435.py
+++ Python/1435.py
@@ -1,37 +1,17 @@
-alignment = 3 #Valor padrão da questão
 while True:
-    tam = int(input())
-    if tam == 0:
-        break
+    n = int(input())
+    if n == 0: break
     
-    arr = []
-    for x in range(tam):
-        arr_2 = []
-        [arr_2.append(x) for _ in range(tam)]
-        arr.append(arr_2)
-    print(arr)
-    """
-    p = 1
-    for x in range(tam):
-        line = ''
-        for y in range(tam):
-            #print(x, y, '(', x+y, ')', sep='', end=' ')
-            if x+y >= tam:
-                line+=(str(p)+'p').rjust(alignment)+' '
-            elif x <= y:
-                line+=(str(p+x)+'x').rjust(alignment)+' '
+    matriz = [[0 for j in range(n)] for i in range(n)]
+    
+    for i in range(n):
+        for j in range(i, n - i):
+            matriz[i][j] = matriz[j][i] = matriz[n-i-1][j] = matriz[j][n-i-1] = i+1
+            
+    for i in range(n):
+        for j in range(n):
+            if j == n - 1:
+                print(f"{matriz[i][j]:3}")
             else:
-                line+=(str(p+y)+'y').rjust(alignment)+' '
-            '''
-            if x<=y and x < 0:
-                line+=str(abs(x)).rjust(alignment)+' '
-            elif x<=y and x >= 0:
-                line+=str(abs(y)).rjust(alignment)+' '
-            elif x>=y and y < 0:
-                line+=str(abs(y)).rjust(alignment)+' '
-            else:
-                line+=str(abs(x)).rjust(alignment)+' '
-            '''
-        #print()
-        print(line[:-1])
-"""
+                print(f"{matriz[i][j]:3}", end=' ')
+    print()
