490240b737f4fe27f34abd2a2be50430f66e6a27
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-04-06 09:15:09 -0300

    Excel Bugado
    É possível otimizar ainda mais o código com base no fórum (https://www.beecrowd.com.br/judge/pt/questions/view/3142/14146)

--- Python/3142.py
+++ Python/3142.py
@@ -1,62 +1,13 @@
-from time import time
-input()
-inicio = time()
-"""
-while True:
-    try:
-        coluna = input()
-        if not coluna: break
-        tamanho = len(coluna)
-        
-        if tamanho > 3 or (tamanho == 3 and coluna > 'XFD'):
-            print('Essa coluna nao existe Tobias!')
-        else:
-            indice = 0
-            multiplicador = tamanho-1
-            for e in coluna:
-                indice+=(ord(e)-64)*26**multiplicador
-                multiplicador-=1
-            print(indice)
-    except EOFError:
-        break
-"""
+hile True:
+    try: coluna = input()
+    except: break
 
-while True:
-    try:
-        coluna = input()
-        if not coluna: break
-        tamanho = len(coluna)
+    tamanho = len(coluna)
+    if (tamanho == 3 and coluna > 'XFD') or tamanho > 3:
+        print('Essa coluna nao existe Tobias!')
+    else:
+        if tamanho == 1: indice = (ord(coluna[0])-64)*(26**(tamanho-1))
+        elif tamanho == 2: indice = (ord(coluna[0])-64)*(26**(tamanho-1)) + (ord(coluna[1])-64)*(26**(tamanho-2))
+        else: indice = (ord(coluna[0])-64)*(26**(tamanho-1)) + (ord(coluna[1])-64)*(26**(tamanho-2)) + (ord(coluna[2])-64)*(26**(tamanho-3))
         
-        if tamanho > 3 or (tamanho == 3 and coluna > 'XFD'):
-            print('Essa coluna nao existe Tobias!')
-        else:
-            indice = 0
-            for x in range(tamanho):
-                indice+=(ord(coluna[x])-64)*26**(tamanho-(x+1))
-            print(indice)
-    except EOFError:
-        break
-
-"""
-D={'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26} 
-while True:
-    try:
-        coluna = input()
-        if not coluna: break
-        tamanho = len(coluna)
-        
-        if tamanho > 3 or (tamanho == 3 and coluna > 'XFD'):
-            print('Essa coluna nao existe Tobias!')
-        else:
-            indice = 0
-            multiplicador = tamanho-1
-            for e in coluna:
-                indice+=D[e]*26**multiplicador
-                multiplicador-=1
-            print(indice)
-    except EOFError:
-        break
-"""
-
-fim = time()
-print(inicio - fim)
+        print(indice)
