9212bbb7dd878273182779eeb9ee95761ca7887b
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-07-25 21:17:26 -0300

    Huaauhahhuahau

--- Python/2598.py
+++ Python/2598.py
@@ -1,5 +1,6 @@
-for _ in range(int(input())):
-    tam, km = map(int, input().split())
-    radares = tam // km
-    if(tam % km != 0): radares += 1
-    print(radares)
+risada = ''
+for l in input():
+    if l in "aeiou": risada += l
+    
+if(risada == risada[::-1]): print("S")
+else: print("N")
