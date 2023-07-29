a48a798574e80d12dfeb346d22a3bb31ab5cccdd
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-07-29 12:09:51 -0300

    Nossos Dias Nunca Voltarão

--- JavaScript/1864.js
+++ JavaScript/1864.js
@@ -1,5 +1,6 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var phrase = 'LIFE IS NOT A PROBLEM TO BE SOLVED';
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
 var len = parseInt(lines.shift());
 
 console.log(phrase.substring(0, len));
