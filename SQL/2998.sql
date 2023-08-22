130856b7ede0e3e831a9d8c5e00ba30d924ddfe7
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-08-22 08:21:16 -0300

    The Payback

--- SQL/2998.sql
+++ SQL/2998.sql
@@ -1,23 +1,10 @@
-SELECT c.name, c.investment, o.month month_of_payback, (so.acumulado - c.investment) return
+SELECT c.name, c.investment, MIN(o.month) as month_of_payback, MIN(o.profit) - c.investment as return
 FROM clients c
-JOIN operations o ON (o.client_id = c.id)
-JOIN 
-    (SELECT o.client_id, o.month, 
-        (SELECT SUM(op.profit) 
-        FROM operations op 
-        WHERE op.month <= o.month AND op.client_id = o.client_id) AS "acumulado"
-    FROM operations o) so ON (so.client_id = c.id AND so.month = o.month)
-WHERE (so.acumulado - c.investment) > 0
--- GROUP BY so.id, c.name, c.investment, o.month, so.acumulado
--- HAVING (so.acumulado - c.investment) > 0
-
-
-SELECT *, 
-    (SELECT SUM(op.profit) 
-    FROM operations op 
-    WHERE op.month <= o.month 
-        AND op.client_id = o.client_id 
-    HAVING (SUM(op.profit) - c.investment) > 0
-    LIMIT 1) AS "acumulado"
-FROM clients c 
-JOIN operations o ON (o.client_id = c.id)
+JOIN (
+    SELECT o.client_id, o.month, SUM(op.profit) profit
+    FROM operations o
+    JOIN operations op ON (op.client_id = o.client_id AND op.month <= o.month)
+    GROUP BY o.client_id, o.month
+) o ON o.client_id = c.id AND o.profit >= c.investment
+GROUP BY c.name, c.investment
+ORDER BY return DESC
