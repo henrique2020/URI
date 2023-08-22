2afee2787512a383d4b6948555ff69d73b8c8fd6
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-08-22 10:49:25 -0300

    Liga

--- SQL/2740.sql
+++ SQL/2740.sql
@@ -1,3 +1,8 @@
-(SELECT CONCAT('Podium: ', team) AS name FROM league ORDER BY position ASC LIMIT 3)
-UNION
-(SELECT CONCAT('Demoted: ', team) AS name FROM league ORDER BY position DESC LIMIT 2)
+SELECT name
+FROM
+(
+    SELECT CONCAT('Podium:', team) AS name, position FROM league LIMIT 3
+    UNION 
+    SELECT CONCAT('Demoted:', team) AS name, position FROM league ORDER BY position DESC LIMIT 2
+) AS league
+ORDER BY position
