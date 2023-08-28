f66f3d674c3f75d961a6a0b8d8a4f2b1536d26ac
Henrique <48599271+henrique2020@users.noreply.github.com>
2023-08-28 18:37:52 -0300

    Máquina de Café

--- Python/2670.py
+++ Python/2670.py
@@ -2,5 +2,9 @@ n1 = int(input())
 n2 = int(input())
 n3 = int(input())
 
-t = [(2*n2 + 4*n3), (2*n1 + 2*n3), (4*n1 + 2*n2)]
-print(min(t))
+if n1+n3 <= 2*n2:
+    print(2*n1 + 2*n3)
+elif n1 > n2 and n1 > n3:
+    print(2*n2 + 4*n3)
+elif n3 > n1 and n3 > n2:
+    print(4*n1 + 2*n2)
