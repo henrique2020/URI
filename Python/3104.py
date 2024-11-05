6dcc67a771a1d3b478ced9df6496e27021c6571d
Henrique <48599271+henrique2020@users.noreply.github.com>
2024-11-05 10:45:09 -0300

    Resto 2.0

--- Python/3104.py
+++ Python/3104.py
@@ -1,42 +1,8 @@
-'''
-  Time limit exceeded
-'''
-#Case 1:
-dividendo = int(input())
-divisao = int(input())
-#print(dividendo%divisao)
-print(divmod(dividendo, divisao)[1])
+a = input()
+b = int(input())
+resto = 0
 
-#Case 2:
-def magicMod(dividendo, divisor):
-    vec = []
-    mod = 0
- 
-    for i in range(0,len(dividendo),1):
-        digit = ord(dividendo[i]) - ord('0')
- 
-        # Update modulo by concatenating
-        # current digit.
-        mod = mod * 10 + digit
- 
-        # Update quotient
-        quo = int(mod / divisor)
-        vec.append(quo)
- 
-        # Update mod for next iteration.
-        mod = mod % divisor    
-     
-    print(mod)
-    
-dividendo = input()
-divisor = int(input())
-magicMod(dividendo, divisor)
+for digito in a:
+	resto = (resto * 10 + int(digito)) % b
 
-'''
-  Runtime error
-'''
-#Case 3:
-from math import remainder
-dividendo = int(input())
-divisao = int(input())
-print(remainder(dividendo, divisao))
+print(resto)
