d5f4cdc815763fc3b494b6758be6ed30d06f8656
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-10-10 20:14:22 -0300

    Salário com Bônus

--- Java/1009.java
+++ Java/1009.java
@@ -1,17 +1,23 @@
+// Problema: 1009 - Salário com Bônus | Resposta: Accepted
+// Linguagem: Java 19 [+2s]           | Tempo: 0.065s
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		InputStreamReader ir = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(ir);
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
+        BufferedReader in = new BufferedReader(ir);
 
-		
-		
-		in.close();
-		ir.close();
-	}
-
+        in.readLine();
+        float salario = Float.parseFloat(in.readLine());
+        float vendas = Float.parseFloat(in.readLine());
+         
+        System.out.printf("TOTAL = R$ %.2f\n", salario + vendas*0.15);
+        
+        in.close();
+        ir.close();
+    }
 }
