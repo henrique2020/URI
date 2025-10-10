fd584eccdc435b70677f624552c7a5bb3af5b9bc
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-10-10 20:15:35 -0300

    Extremamente Básico

--- Java/1001.java
+++ Java/1001.java
@@ -1,13 +1,16 @@
+// Problema: 1001 - Extremamente Básico | Resposta: Accepted
+// Linguagem: Java 8 [+2s]              | Tempo: 0.088s
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		InputStreamReader ir = new InputStreamReader(System.in);
+    public static void main(String[] args) throws IOException {
+        InputStreamReader ir = new InputStreamReader(System.in);
         BufferedReader in = new BufferedReader(ir);
-    	        
+
         int A = 0, B = 0, X = 0;
          
         A = Integer.parseInt(in.readLine());
@@ -20,5 +23,4 @@ public class Main {
         in.close();
         ir.close();
     }
-	     
 }
