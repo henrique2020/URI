5dcb5ae4f525028da91186badf4b7c5cb4ca53b4
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-10-28 16:41:14 -0300

    Tira-teima

---
+++ Python/2424.py
@@ -0,0 +1,6 @@
+# Problema: 2424 - Tira-teima  | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s] | Tempo: 0.055s
+
+x, y = map(int, input().split())
+if 0 <= x <= 432 and 0 <= y <= 468: print('dentro')
+else: print('fora')
