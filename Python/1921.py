e55f2b126b659afc298cbbfb06117cd242547c1e
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-10-31 20:07:08 -0300

    Guilherme e Suas Pipas

---
+++ Python/1921.py
@@ -0,0 +1,5 @@
+# Problema: 1921 - Guilherme e Suas Pipas | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s]            | Tempo: 0.000s
+
+lados = int(input())
+print(f"{(lados-3)*(lados/2):.0f}")
