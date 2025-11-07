10d4f0889826a38f7bd73a79ee3208c3f44327b7
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-11-07 12:10:27 -0300

    I am Toorg!

---
+++ PHP/2581.php
@@ -0,0 +1,7 @@
+<?php
+// Problema: 2581 - I am Toorg! | Resposta: Accepted
+// Linguagem: PHP (8.1.2) [+1s] | Tempo: 0.000s
+
+$limite = intval(fgets(STDIN));
+for($i = 0; $i < $limite; $i++){ echo "I am Toorg!\n"; }
+?>
