c62a16b03c97ab9fd2df0944baf5f731ff03c63e
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-11-21 10:53:07 -0300

    Função Binária

--- Python/1593.py
+++ Python/1593.py
@@ -3,6 +3,4 @@
 
 for _ in range(int(input())):
     dec = int(input())
-    number_bin = bin(dec)[2:]
-    str_bin = str(number_bin)
-    print(str_bin.count('1'))
+    print(f'{dec:b}'.count('1'))
