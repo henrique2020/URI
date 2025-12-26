a88bd7148a88d7d18a668b2dfbb9b39270be2e17
Henrique <48599271+henrique2020@users.noreply.github.com>
2025-12-26 14:41:06 -0300

    Trilhos

--- Python/1062.py
+++ Python/1062.py
@@ -1,43 +1,31 @@
 # Problema: 1062 - Trilhos     | Resposta: Accepted
-# Linguagem: Python 3.11 [+1s] | Tempo: 1.159s
+# Linguagem: Python 3.11 [+1s] | Tempo: 0.442s
 
-vagoes = int(input())
-while (vagoes != 0):
+while True:
+    vagoes = int(input())
+    if vagoes == 0: break
+    
     while True:
-        entrada = [n for n in range(1, vagoes+1)]
         gabarito = list(map(int, input().split()))
         if len(gabarito) == 1 and gabarito[0] == 0:
             print()
             break
         
-        elif(entrada == gabarito or entrada == gabarito[::-1]):
-            print("Yes")
+        parado = []
+        proximo_vagao = 1
+        possivel = True
         
-        else:
-            saida = []
-            parado = []
+        for esperado in gabarito:
+            while proximo_vagao <= vagoes and (not parado or parado[-1] != esperado):
+                parado.append(proximo_vagao)
+                proximo_vagao += 1
             
-            p = 0
-            while p < vagoes:
-                if len(entrada) > 0:
-                    e = entrada[0]
-                    entrada.remove(e)
-                else: e = -1
+            if parado and parado[-1] == esperado:
+                parado.pop()
                 
-                if e == gabarito[p]: 
-                    saida.append(e)
-                elif gabarito[p] in parado and parado[-1] == gabarito[p]:
-                    saida.append(parado[-1])
-                    parado.remove(parado[-1])
-                    entrada.insert(0, e)
-                elif e != -1:
-                    parado.append(e)
-                    continue
-                
-                p += 1
-                    
-            
-            if(gabarito == saida): print("Yes")
-            else: print("No")
-    
-    vagoes = int(input())
+            else:
+                possivel = False
+                break
+        
+        if possivel: print("Yes")
+        else: print("No")
